e240d893d095494c04a142dc9e8251197fbc2264
Prashan Fernadno <prashanf@labfriend.com>
2021-09-10 01:56:25 +0530

    update

--- pages/api/forward-to-slack.ts
+++ pages/api/forward-to-slack.ts
@@ -3,14 +3,11 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { GetSlackMessages } from 'src/utils/hook-transform';
 import { PostToSlack } from 'src/utils/slack-helper';
 
-type Data = {
-  name: string
-}
 
 const slackToken = process.env.SLACK_TOKEN;
 
-export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const messages = GetSlackMessages(req.body);
+export default async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  const messages: any = GetSlackMessages(req.body);
   await Promise.all(messages.map(PostToSlack));
   res.status(200).json({ messages })
 }
