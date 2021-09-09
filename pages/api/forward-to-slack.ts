2caeae228ac75fba964e7dc450fe0c27479be25e
Prashan Fernadno <prashanf@labfriend.com>
2021-09-09 22:48:04 +0530

    update

--- pages/api/forward-to-slack.ts
+++ pages/api/forward-to-slack.ts
@@ -1,10 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import  { WebClient }  from '@slack/web-api';
 
 type Data = {
   name: string
 }
 
+const slackToken = process.env.SLACK_TOKEN;
+
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: `John Doe ${slackToken}` })
 }
